d2e49dce86d7585720c46a1dd8a6bf5b9c2bb472
Krisztian Godrei <godrekrisztian@gmil.com>
2015-11-19 16:23:48 +0000

    fix

--- step.sh
+++ step.sh
@@ -8,12 +8,12 @@ ruby "${THIS_SCRIPTDIR}/step.rb" \
   -c "${xamarin_configuration}" \
   -p "${xamarin_platform}" \
   -i "${is_clean_build}" \
-  -a "${api_key}" \
-  -u "${user}" \
-  -d "${devices}" \
-  -n "${app_name}" \
-  -y "${async}" \
-  -e "${category}" \
-  -f "${fixture}" \
-  -r "${series}" \
-  -l "${parallelization}"
+  -a "${test_cloud_api_key}" \
+  -u "${xamarin_user}" \
+  -d "${test_cloud_devices}" \
+  -n "${test_cloud_app_name}" \
+  -y "${test_cloud_is_async}" \
+  -e "${test_cloud_category}" \
+  -f "${test_cloud_fixture}" \
+  -r "${test_cloud_series}" \
+  -l "${test_cloud_parallelization}"
